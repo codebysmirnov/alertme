11a0d658fe9179608f134d95e6a07c466f8be62c
codebysmirnov <codebysmirnov@gmail.com>
2024-01-26 20:43:54 +0300

    Decompose code to separated methods

--- main.go
+++ main.go
@@ -20,6 +20,125 @@ const (
 	appName = "AlertMe"
 )
 
+type RestNotifier struct {
+	App               fyne.App
+	Window            fyne.Window
+	Ticker            *time.Ticker
+	StopTicker        chan struct{}
+	StartNextTicker   chan struct{}
+	SkipTicker        chan struct{}
+	TotalRestTime     time.Duration
+	TotalRestLabel    *widget.Label
+	RestDurationLabel *widget.Label
+	IntervalMinutes   int
+}
+
+func NewRestNotifier(intervalMinutes int) *RestNotifier {
+	return &RestNotifier{
+		App:               app.NewWithID(appName),
+		Ticker:            time.NewTicker(time.Duration(intervalMinutes) * time.Minute),
+		StopTicker:        make(chan struct{}),
+		StartNextTicker:   make(chan struct{}),
+		SkipTicker:        make(chan struct{}),
+		TotalRestTime:     time.Duration(0),
+		TotalRestLabel:    widget.NewLabel("Total Rest Time: 0s"),
+		RestDurationLabel: widget.NewLabel("Rest Duration: 0s"),
+		IntervalMinutes:   intervalMinutes,
+	}
+}
+
+func (rn *RestNotifier) InitializeWindow() {
+	rn.Window = rn.App.NewWindow("Rest time")
+	rn.Window.Resize(fyne.NewSize(300, 200))
+	rn.Window.SetFixedSize(true)
+	rn.Window.CenterOnScreen()
+	rn.Window.SetIcon(theme.FyneLogo())
+}
+
+func (rn *RestNotifier) SetupSystemTrayMenu() {
+	if desk, ok := rn.App.(desktop.App); ok {
+		m := fyne.NewMenu("Main",
+			fyne.NewMenuItem("Start rest", func() {
+				log.Println("rest started")
+			}))
+		desk.SetSystemTrayMenu(m)
+	}
+}
+
+func (rn *RestNotifier) SetupUI() {
+	continueButton := widget.NewButton("Continue", func() {
+		rn.StopTicker <- struct{}{}      // Send a signal to stop the current timer
+		rn.StartNextTicker <- struct{}{} // Send a signal to start the next timer
+		log.Println("Continue button pressed")
+	})
+
+	skipButton := widget.NewButton("Skip", func() {
+		rn.SkipTicker <- struct{}{}      // Send a signal to skip the rest
+		rn.StartNextTicker <- struct{}{} // Send a signal to start the next timer
+		log.Println("Skip button pressed")
+	})
+
+	exitButton := widget.NewButton("Exit", func() {
+		rn.StopTicker <- struct{}{} // Send a signal to stop the current timer
+		log.Println("Exit button pressed")
+		dialog.ShowInformation("Rest Timer", "Program interrupted", rn.Window)
+		os.Exit(0) // Exit the program
+	})
+
+	restNotificationText := widget.NewLabel("You need to rest! Press 'Continue button' after the rest")
+	rn.Window.SetContent(container.NewVBox(
+		restNotificationText,
+		continueButton,
+		skipButton,
+		exitButton,
+		rn.TotalRestLabel,
+		rn.RestDurationLabel,
+	))
+}
+
+func (rn *RestNotifier) ShowNotification() {
+	startTime := time.Now() // Record the start time of the rest
+	rn.Window.Show()
+	log.Println("Notification appeared")
+	rn.Ticker.Stop()
+
+	go func() {
+		for {
+			select {
+			case <-rn.StopTicker: // Wait for a signal to stop the current timer
+				rn.Window.Hide()
+				endTime := time.Now()                 // Record the time when the Continue or Skip button is pressed
+				elapsedTime := endTime.Sub(startTime) // Calculate the elapsed time
+				rn.TotalRestTime += elapsedTime       // Add the elapsed time to the total rest time
+				log.Printf("Rest duration: %v\n", elapsedTime)
+				log.Printf("Total rest time: %v\n", rn.TotalRestTime)
+				// Update UI with the total rest time
+				rn.TotalRestLabel.SetText(fmt.Sprintf("Total Rest Time: %s", rn.TotalRestTime.Round(time.Second).String()))
+				return
+			case <-rn.SkipTicker: // Wait for a signal to skip the rest
+				rn.Window.Hide()
+				return
+			default:
+				elapsed := time.Since(startTime).Round(time.Second)
+				// Update UI with the current rest duration
+				rn.RestDurationLabel.SetText(fmt.Sprintf("Rest Duration: %s", elapsed.String()))
+				time.Sleep(time.Second)
+			}
+		}
+	}()
+}
+
+func (rn *RestNotifier) Run() {
+	for {
+		select {
+		case <-rn.Ticker.C:
+			rn.ShowNotification()
+		case <-rn.StartNextTicker:
+			rn.Ticker = time.NewTicker(time.Duration(rn.IntervalMinutes) * time.Minute) // Start a new timer
+		}
+	}
+}
+
 func main() {
 	var intervalMinutes int
 	flag.IntVar(&intervalMinutes, "interval", 25, "Rest timer interval in minutes")
@@ -27,108 +146,18 @@ func main() {
 
 	log.Printf("Rest timer interval set to %d minutes\n", intervalMinutes)
 
-	ticker := time.NewTicker(time.Duration(intervalMinutes) * time.Minute)
-	stopTicker := make(chan struct{})      // Channel to stop the current timer
-	startNextTicker := make(chan struct{}) // Channel to start the next timer
-	skipTicker := make(chan struct{})      // Channel to skip the rest
-	totalRestTime := time.Duration(0)      // Variable to count the total rest time
-
-	App := app.NewWithID(appName)
-	// enable the tray menu if this is a desktop application
-	if desk, ok := App.(desktop.App); ok {
-		m := fyne.NewMenu("Main",
-			fyne.NewMenuItem("Start rest", func() {
-				log.Println("rest started")
-			}))
-		desk.SetSystemTrayMenu(m)
-	}
-
-	w := App.NewWindow("Rest time")
-	w.Resize(fyne.NewSize(300, 200))
-	w.SetFixedSize(true)
-	w.CenterOnScreen()
-	w.SetIcon(theme.FyneLogo())
-
-	continueButton := widget.NewButton("Continue", func() {
-		w.Hide()
-		stopTicker <- struct{}{}      // Send a signal to stop the current timer
-		startNextTicker <- struct{}{} // Send a signal to start the next timer
-		log.Println("Continue button pressed")
-	})
-
-	skipButton := widget.NewButton("Skip", func() {
-		w.Hide()
-		skipTicker <- struct{}{}      // Send a signal to skip the rest
-		startNextTicker <- struct{}{} // Send a signal to start the next timer
-		log.Println("Skip button pressed")
-	})
-
-	totalRestLabel := widget.NewLabel("Total Rest Time: 0s")
-	restDurationLabel := widget.NewLabel("Rest Duration: 0s")
-
-	exitButton := widget.NewButton("Exit", func() {
-		w.Hide()
-		stopTicker <- struct{}{} // Send a signal to stop the current timer
-		log.Println("Exit button pressed")
-		dialog.ShowInformation("Rest Timer", "Program interrupted", w)
-		os.Exit(0) // Exit the program
-	})
-
-	restNotificationText := widget.NewLabel("You need to rest! Press 'Continue button' after the rest")
-	w.SetContent(container.NewVBox(
-		restNotificationText,
-		continueButton,
-		skipButton,
-		exitButton,
-		totalRestLabel,
-		restDurationLabel,
-	))
-
-	go func() {
-		for {
-			select {
-			case <-ticker.C:
-				startTime := time.Now() // Record the start time of the rest
-				w.Show()
-				log.Println("Notification appeared")
-
-				go func() {
-					for {
-						select {
-						case <-stopTicker: // Wait for a signal to stop the current timer
-							w.Hide()
-							endTime := time.Now()                 // Record the time when the Continue or Skip button is pressed
-							elapsedTime := endTime.Sub(startTime) // Calculate the elapsed time
-							totalRestTime += elapsedTime          // Add the elapsed time to the total rest time
-							log.Printf("Rest duration: %v\n", elapsedTime)
-							log.Printf("Total rest time: %v\n", totalRestTime)
-							totalRestLabel.SetText(fmt.Sprintf("Total Rest Time: %s", totalRestTime.Round(time.Second).String()))
-							return
-						case <-skipTicker: // Wait for a signal to skip the rest
-							w.Hide()
-							return
-						default:
-							elapsed := time.Since(startTime).Round(time.Second)
-							restDurationLabel.SetText(fmt.Sprintf("Rest Duration: %s", elapsed.String()))
-							time.Sleep(time.Second)
-						}
-					}
-				}()
-
-				<-startNextTicker                                                     // Wait for a signal to start the next timer
-				ticker = time.NewTicker(time.Duration(intervalMinutes) * time.Minute) // Start a new timer
-			case <-startNextTicker: // Wait for a signal to start the next timer
-				ticker = time.NewTicker(time.Duration(intervalMinutes) * time.Minute) // Start a new timer
-			}
-		}
-	}()
+	rt := NewRestNotifier(intervalMinutes)
+	rt.InitializeWindow()
+	rt.SetupSystemTrayMenu()
+	rt.SetupUI()
 
 	log.Println("Rest timer program successfully started!")
 
 	defer func() {
-		log.Printf("Total rest time during the program: %v\n", totalRestTime)
+		log.Printf("Total rest time during the program: %v\n", rt.TotalRestTime)
 		log.Println("Rest timer program successfully terminated!")
 	}()
 
-	App.Run()
+	go rt.Run()
+	rt.App.Run()
 }
